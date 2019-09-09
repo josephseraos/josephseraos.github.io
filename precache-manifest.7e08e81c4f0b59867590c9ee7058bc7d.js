5958bada2fa72efa89958e2e3a30667a4d33ae44
Joseph Soares Alcântara <josephseraos@gmail.com>
2019-09-09 11:51:03 -0300

    Primeiro build do site feito com React

---
+++ precache-manifest.7e08e81c4f0b59867590c9ee7058bc7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fab144e96699f9647e5e",
+    "url": "/static/js/main.ce2841cf.chunk.js"
+  },
+  {
+    "revision": "033218dcfde842a96bec",
+    "url": "/static/js/2.7c5a8252.chunk.js"
+  },
+  {
+    "revision": "fab144e96699f9647e5e",
+    "url": "/static/css/main.a37a98b8.chunk.css"
+  },
+  {
+    "revision": "380feb7dd7816e7271a1664bba75f128",
+    "url": "/index.html"
+  }
+];
